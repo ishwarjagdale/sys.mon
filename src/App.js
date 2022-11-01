cce8fe40b8647428d29956d493509bbc5e707066
Ishwar Jagdale <ishwarrules@gmail.com>
2022-11-02 00:15:45 +0530

    revised Form.js to Auth.js, nested routes and implemented forgot password

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import Navigation from "./components/Navigation";
-import HeroSection from "./components/HeroSection";
+import Home from "./views/Home";
 import Footer from "./components/Footer";
 import {useState} from "react";
 import "./App.css";
+import {Outlet} from 'react-router-dom';
+import heroImage from "./img/2794209.jpg";
 
-function App({page=0}) {
-
-    const [currentPage, setPage] = useState(page);
-
+function App() {
     return (
         <>
-            <Navigation setPage={setPage}/>
-            <HeroSection page={currentPage} setPage={setPage}/>
+            <Navigation/>
+            <div id={"hero-section"} className={`flex w-full lg:w-4/5 lg:mx-auto items-center relative overflow-hidden flex-shrink`}>
+                <Outlet/>
+                <img id={"heroImage"} src={heroImage} className={"hidden lg:block md:max-w-[50%] px-4"} alt={"process"} title={"Designed by slidesgo / Freepik"}/>
+            </div>
             <Footer/>
         </>
     );
--- src/index.js
+++ src/index.js
@@ -8,23 +8,38 @@ import {
 } from "react-router-dom";
 import Oops from "./views/oops";
 import Dashboard from "./views/Dashboard/Dashboard";
+import Home from "./views/Home";
+import Auth from "./views/Auth";
 
 const router = createBrowserRouter([
     {
-        path: "/",
-        element: <App/>
-    }, {
-        path: "/get-started",
-        element: <App page={1}/>
-    }, {
-        path: "/login",
-        element: <App page={2}/>
-    }, {
-        path: "/forgot-password",
-        element: <App page={3}/>
-    }, {
-        path: "/verification",
-        element: <App page={4}/>
+        element: <App/>,
+        children: [
+            {
+                path: '/',
+                element: <Home/>
+            },
+            {
+                path: '/get-started',
+                element: <Auth page={1}/>
+            },
+            {
+                path: '/login',
+                element: <Auth page={2}/>
+            },
+            {
+                path: '/forgot-password',
+                element: <Auth page={3}/>
+            },
+            {
+                path: '/forgot-password/:resetToken',
+                element: <Auth page={3}/>
+            },
+            {
+                path: '/verification',
+                element: <Auth page={4}/>
+            }
+        ]
     }, {
         path: "/oops",
         element: <Oops/>
@@ -37,6 +52,6 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <RouterProvider router={router}/>
+        <RouterProvider router={router} errorElement={<Oops/>}/>
     </React.StrictMode>
 );
